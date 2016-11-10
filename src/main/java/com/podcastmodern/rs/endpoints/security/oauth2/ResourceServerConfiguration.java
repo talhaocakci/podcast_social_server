a9cb5d588f39539df22980c1acde49c1e2332c0b
Talha Ocakci <talha.ocakci@monitise.com>
2016-11-10 17:06:55 +0300

    application endoint is protected

--- src/main/java/com/podcastmodern/rs/endpoints/security/oauth2/ResourceServerConfiguration.java
+++ src/main/java/com/podcastmodern/rs/endpoints/security/oauth2/ResourceServerConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
 @Configuration
 @EnableResourceServer
@@ -28,15 +29,13 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        /*
-        /application/** yazacaksın, simdilik hic kontrol yapılmasın.
-         */
-       /* http.
+
+        http.
             anonymous().disable()
             .requestMatchers().antMatchers("/application/**")
             .and().authorizeRequests()
             .antMatchers("/application/**").access("hasRole('ADMIN')")
-            .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());*/
+            .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
     }
 
 }
